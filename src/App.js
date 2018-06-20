1bd6848db1b2820b20cc74de58ce50059c51ee35
petercolella <peter.joseph.colella@gmail.com>
2018-06-19 22:56:25 -0400

    Lecture 64

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'; // eslint-disable-line no-unused-vars
 import './App.css';
+import Radium from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -44,10 +45,15 @@ class App extends Component {
   render() {
     const style = {
       backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -66,6 +72,10 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     const classes = [];
@@ -90,4 +100,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
