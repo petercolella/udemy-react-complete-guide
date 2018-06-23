1668ac176cfc2917f2dcf79bcb782f8719ee55a5
petercolella <peter.joseph.colella@gmail.com>
2018-06-22 23:32:14 -0400

    Lecture 79

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'; // eslint-disable-line no-unused-vars
 import classes from './App.css';
-import Person from './Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
+import Person from '../components/Persons/Person/Person';
 
 class App extends Component {
   state = {
@@ -50,13 +49,12 @@ class App extends Component {
       persons = (
         <div >
           {this.state.persons.map((person, index) => {
-            return <ErrorBoundary key={person.id}>
-              <Person 
+            return <Person 
               click={() => this.deletePersonHandler(index)}
               name={person.name} 
               age={person.age}
+              key={person.id}
               changed={(event) => this.nameChangeHandler(event, person.id)} />
-            </ErrorBoundary>
           })}
         </div>
       );
