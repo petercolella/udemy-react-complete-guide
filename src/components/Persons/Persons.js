73c58529f10d3e8832ec590f6cc73498cdf3ccd6
petercolella <peter.joseph.colella@gmail.com>
2018-06-24 00:07:20 -0400

    Lecture 83

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -6,11 +6,11 @@ class Persons extends Component {
     render () {
         return this.props.persons.map( (person, index) => {
             return <Person 
-                click={() => props.clicked(index)}
+                click={() => this.props.clicked(index)}
                 name={person.name} 
                 age={person.age}
                 key={person.id}
-                changed={(event) => props.changed(event, person.id)} />
+                changed={(event) => this.props.changed(event, person.id)} />
           } );
     }
 }
