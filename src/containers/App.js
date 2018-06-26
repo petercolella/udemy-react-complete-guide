7524b489ffa5f4f139d2701447f6b5861bf6381f
petercolella <peter.joseph.colella@gmail.com>
2018-06-25 23:52:42 -0400

    Lecture 94

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import React, { PureComponent } from 'react'; // eslint-disable-line no-unused-v
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 class App extends PureComponent {
   constructor(props) {
@@ -91,7 +92,7 @@ class App extends PureComponent {
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
         <Cockpit
           appTitle={this.props.title}
@@ -99,7 +100,7 @@ class App extends PureComponent {
           persons={this.state.persons}
           clicked={this.togglePersonsHandler} />
         {persons}
-      </div>
+      </WithClass>
     );
     // return React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'Does this work now?'))
   }
