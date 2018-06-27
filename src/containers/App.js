a685da3b2a93317f49a367431c9c127e09352ec0
petercolella <peter.joseph.colella@gmail.com>
2018-06-26 23:45:11 -0400

    Lecture 98

--- src/containers/App.js
+++ src/containers/App.js
@@ -17,7 +17,8 @@ class App extends PureComponent {
         {id: 'ab3', name: 'Carolyn', age: 49}
       ],
       otherState: 'some other value',
-      showPersons: false
+      showPersons: false,
+      toggleClicked: 0
     };    
   }
 
@@ -78,7 +79,12 @@ class App extends PureComponent {
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow});
+    this.setState( (prevState, props) => {
+      return {
+        showPersons: !doesShow,
+        toggleClicked: prevState.toggleClicked + 1
+      }
+    });
   }
 
   render() {
