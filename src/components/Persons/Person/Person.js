d39ab1fa08d31c8f1725198f8c9cc4018db56e9d
petercolella <peter.joseph.colella@gmail.com>
2018-06-27 20:54:54 -0400

    Lecture 99

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import classes from './Person.css';
 import withClass from '../../../hoc/withClass';
 import Aux from '../../../hoc/Aux';
@@ -33,6 +35,13 @@ class Person extends Component {
 
         // ]
     }
+}
+
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
 }
 
 export default withClass(Person, classes.Person);
