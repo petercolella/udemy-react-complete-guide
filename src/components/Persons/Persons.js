7c04323865d65c55d7bc1bba57e3d0c9cd6fe247
petercolella <peter.joseph.colella@gmail.com>
2018-06-27 21:10:44 -0400

    Lecture 101

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -41,6 +41,7 @@ class Persons extends PureComponent {
         return this.props.persons.map( (person, index) => {
             return <Person 
                 click={() => this.props.clicked(index)}
+                position={index}
                 name={person.name} 
                 age={person.age}
                 key={person.id}
