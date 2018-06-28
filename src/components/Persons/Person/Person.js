a17e153372c51ec7f6f611c13389a8b13836c210
petercolella <peter.joseph.colella@gmail.com>
2018-06-27 21:48:26 -0400

    Lecture 102

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -9,6 +9,7 @@ class Person extends Component {
     constructor(props) {
         super(props);
         console.log('[Person.js] Inside Constructor', props);
+        this.inputElement = React.createRef();
     }
 
     UNSAFE_componentWillMount() {
@@ -18,8 +19,12 @@ class Person extends Component {
     componentDidMount() {
         console.log('[Person.js] Inside componentDidMount');
         if (this.props.position === 0) {
-            this.inputElement.focus();
+            this.inputElement.current.focus();
         }
+    }
+
+    focus() {
+        this.inputElement.current.focus();
     }
 
     render () {
@@ -29,7 +34,7 @@ class Person extends Component {
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input 
-                    ref={(inp) => { this.inputElement = inp }}
+                    ref={this.inputElement}
                     type="text" 
                     onChange={this.props.changed} 
                     value={this.props.name}/>
