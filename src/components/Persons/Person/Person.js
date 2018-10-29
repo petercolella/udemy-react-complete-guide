fa7d6caa1bda2a10cfab41e4c931b1158b1c545e
petercolella <peter.joseph.colella@gmail.com>
2018-10-28 22:55:43 -0400

    Lecture 103.1

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -7,57 +7,67 @@ import Aux from '../../../hoc/Aux';
 import { AuthContext } from '../../../containers/App';
 
 class Person extends Component {
-    constructor(props) {
-        super(props);
-        console.log('[Person.js] Inside Constructor', props);
-        this.inputElement = React.createRef();
-    }
+  constructor(props) {
+    super(props);
+    console.log('[Person.js] Inside Constructor', props);
+    this.inputElement = React.createRef();
+  }
 
-    UNSAFE_componentWillMount() {
-        console.log('[Person.js] Inside componentWillMount()');
-    }
+  UNSAFE_componentWillMount() {
+    console.log('[Person.js] Inside componentWillMount()');
+  }
 
-    componentDidMount() {
-        console.log('[Person.js] Inside componentDidMount');
-        if (this.props.position === 0) {
-            this.inputElement.current.focus();
-        }
+  componentDidMount() {
+    console.log('[Person.js] Inside componentDidMount');
+    if (this.props.position === 0) {
+      this.inputElement.current.focus();
     }
+  }
 
-    focus() {
-        this.inputElement.current.focus();
-    }
+  focus() {
+    this.inputElement.current.focus();
+  }
 
-    render () {
-        console.log('[Person.js] Inside render()');
-        return (
-            <Aux>
-                <AuthContext.Consumer>
-                    {auth => auth ? <p>I'm authenticated</p> : null}
-                </AuthContext.Consumer>
-                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
-                <p>{this.props.children}</p>
-                <input 
-                    ref={this.inputElement}
-                    type="text" 
-                    onChange={this.props.changed} 
-                    value={this.props.name}/>
-            </Aux>
-        )
-        // return [
-        //     <p key="1" onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>,
-        //     <p key="2">{this.props.children}</p>,
-        //     <input key="3" type="text" onChange={this.props.changed} value={this.props.name}/>
+  //   focusInput() {
+  //     if (this.props.position === 0) {
+  //       this.inputElement.current.focus();
+  //     }
+  //   }
 
-        // ]
-    }
+  render() {
+    console.log('[Person.js] Inside render()');
+    return (
+      <Aux>
+        <AuthContext.Consumer>
+          {auth => (auth ? <p>I'm authenticated</p> : null)}
+        </AuthContext.Consumer>
+        <p onClick={this.props.click}>
+          I'm {this.props.name} and I am {this.props.age} years old!
+        </p>
+        <p>{this.props.children}</p>
+        <input
+          ref={this.inputElement}
+          // ref={(inp) => {this.inputElement = inp}}
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
+      </Aux>
+    );
+    // return [
+    //     <p key="1" onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>,
+    //     <p key="2">{this.props.children}</p>,
+    //     <input key="3" type="text" onChange={this.props.changed} value={this.props.name}/>
+
+    // ]
+  }
 }
 
 Person.propTypes = {
-    click: PropTypes.func,
-    name: PropTypes.string,
-    age: PropTypes.number,
-    changed: PropTypes.func
-}
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
+};
 
 export default withClass(Person, classes.Person);
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -3,54 +3,64 @@ import React, { PureComponent } from 'react';
 import Person from './Person/Person';
 
 class Persons extends PureComponent {
-    constructor(props) {
-        super(props);
-        console.log('[Persons.js] Inside Constructor', props);
-        this.lastPersonRef = React.createRef();
-    }
+  constructor(props) {
+    super(props);
+    console.log('[Persons.js] Inside Constructor', props);
+    this.lastPersonRef = React.createRef();
+  }
 
-    UNSAFE_componentWillMount() {
-        console.log('[Persons.js] Inside componentWillMount()');
-    }
+  UNSAFE_componentWillMount() {
+    console.log('[Persons.js] Inside componentWillMount()');
+  }
 
-    componentDidMount() {
-        console.log('[Persons.js] Inside componentDidMount()');
-        this.lastPersonRef.current.focus();
-    }
+  componentDidMount() {
+    console.log('[Persons.js] Inside componentDidMount()');
+    this.lastPersonRef.current.focus();
+  }
 
-    UNSAFE_componentWillReceiveProps(nextProps) {
-        console.log('[UPDATE Persons.js] Inside componentWillReceiveProps', nextProps);
-    }
+  UNSAFE_componentWillReceiveProps(nextProps) {
+    console.log(
+      '[UPDATE Persons.js] Inside componentWillReceiveProps',
+      nextProps
+    );
+  }
 
-    // shouldComponentUpdate(nextProps, nextState) {
-    //     console.log('[UPDATE Persons.js] Inside shouldComponentUpdate', nextProps, nextState);
-    //     return nextProps.persons !== this.props.persons || 
-    //         nextProps.changed !== this.props.changed ||
-    //         nextProps.clicked !== this.props.clicked;
-    //     // return true;
-    // }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //     console.log('[UPDATE Persons.js] Inside shouldComponentUpdate', nextProps, nextState);
+  //     return nextProps.persons !== this.props.persons ||
+  //         nextProps.changed !== this.props.changed ||
+  //         nextProps.clicked !== this.props.clicked;
+  //     // return true;
+  // }
 
-    UNSAFE_componentWillUpdate(nextProps, nextState) {
-        console.log('[UPDATE Persons.js] Inside componentWillUpdate', nextProps, nextState);
-    }
+  UNSAFE_componentWillUpdate(nextProps, nextState) {
+    console.log(
+      '[UPDATE Persons.js] Inside componentWillUpdate',
+      nextProps,
+      nextState
+    );
+  }
 
-    componentDidUpdate() {
-        console.log('[UPDATE Persons.js] Inside componentDidUpdate');
-    }
+  componentDidUpdate() {
+    console.log('[UPDATE Persons.js] Inside componentDidUpdate');
+  }
 
-    render () {
-        console.log('[Persons.js] Inside render()');
-        return this.props.persons.map( (person, index) => {
-            return <Person 
-                click={() => this.props.clicked(index)}
-                position={index}
-                name={person.name} 
-                age={person.age}
-                ref={this.lastPersonRef}
-                key={person.id}
-                changed={(event) => this.props.changed(event, person.id)} />
-          } );
-    }
+  render() {
+    console.log('[Persons.js] Inside render()');
+    return this.props.persons.map((person, index) => {
+      return (
+        <Person
+          click={() => this.props.clicked(index)}
+          position={index}
+          name={person.name}
+          age={person.age}
+          ref={this.lastPersonRef}
+          key={person.id}
+          changed={event => this.props.changed(event, person.id)}
+        />
+      );
+    });
+  }
 }
 
 export default Persons;
